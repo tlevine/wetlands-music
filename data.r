04f51c92bc52cb99061375c264a8081d3269025e
Thomas Levine <occurrence@thomaslevine.com>
2013-06-09 07:47:31 -0700

    load data from database

--- data.r
+++ data.r
@@ -1,5 +1,9 @@
 library(sqldf)
 if (!('applications' %in% ls())){
   # applications <- read.csv('http://scott.thomaslevine.com/applications.csv')
-  applications <- sqldf('SELECT "permitApplicationNumber", "projectDescription", "projectManagerName", "publicNoticeDate", "expirationDate", "publicNoticeUrl", "drawingsUrl", "parish", "longitude", "latitude", "acreage", "type" FROM application', dbname = 'applications.db')
+  applications <- sqldf(
+    'SELECT "permitApplicationNumber", "projectDescription", "projectManagerName", "publicNoticeDate", "expirationDate", "publicNoticeUrl", "drawingsUrl", "parish", "longitude", "latitude", "acreage", "type" FROM application',
+    dbname = 'applications.db',
+    method = c('character', 'character', 'factor', 'Date2', 'Date2', 'character', 'character', 'factor', 'numeric', 'numeric', 'numeric', 'factor'),
+  )
 }
