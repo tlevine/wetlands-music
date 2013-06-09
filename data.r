bce2cf8cd9137bd0e208752fb567f66836bfdb32
Thomas Levine <occurrence@thomaslevine.com>
2013-06-09 07:40:47 -0700

    typos

--- data.r
+++ data.r
@@ -1,5 +1,5 @@
-library(sqldf)b
+library(sqldf)
 if (!('applications' %in% ls())){
   # applications <- read.csv('http://scott.thomaslevine.com/applications.csv')
-  applications <- sqldf('SELECT "permitApplicationNumber", "projectDescription", "projectManagerName", "publicNoticeDate", "expirationDate", "publicNoticeUrl", "drawingsUrl", "parish", "longitude", "latitude", "acreage", "type" FROM application', dbname = applications.db)
+  applications <- sqldf('SELECT "permitApplicationNumber", "projectDescription", "projectManagerName", "publicNoticeDate", "expirationDate", "publicNoticeUrl", "drawingsUrl", "parish", "longitude", "latitude", "acreage", "type" FROM application', dbname = 'applications.db')
 }
